25859e788f1b6aae5aecebdb5064d835ef0a8ae8
Liam Falconer <falconerl@lmax.com>
2018-01-26 15:57:31 +1300

    Replace the immediate-children-of hint applicability.
    Replaced with a stash on the source that gets applied to the next
    interval we push onto the stack.

--- src/main/java/net/lfn3/undertaker/junit/sources/ReflectiveSource.java
+++ src/main/java/net/lfn3/undertaker/junit/sources/ReflectiveSource.java
@@ -1,8 +1,14 @@
 package net.lfn3.undertaker.junit.sources;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public interface ReflectiveSource {
     <T> T reflectively(Class<T> klass);
     <T> T reflectively(Constructor<T> constructor);
+    <T> T reflectively(Function<?, T> function);
+    <T> T reflectively(BiFunction<?, ?, T> function);
+    <T> T reflectively(Method function);
 }
