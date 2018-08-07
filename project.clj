16c915c4072007279e8b54010f13a446aac2fd96
Liam <liam@lfn3.net>
2018-08-07 09:27:16 +0100

    Release v0.1.5
    Headline features are:
    
    - `source.reflective(...)` can now generate generics (when it can find concrete type arguments in the chain of constructors it's using).
    - `source.reflective(...)` will now use static factory methods when available as well as constructors.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.lfn3/undertaker-junit "0.1.5-SNAPSHOT"
+(defproject net.lfn3/undertaker-junit "0.1.5"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"
